4f75d670a6580dbcb0c8c728bf96a23a43eacbfa
suffyduffy <elfiansufi@gmail.com>
2024-03-16 23:58:22 +0800

    some changes to links

--- home.php
+++ home.php
@@ -19,7 +19,7 @@
             <div class="content">
                <span>Popular!</span>
                <h3>Beef lasagna</h3>
-               <a href="menu.html" class="btn">Menu</a>
+               <a href="menu.php" class="btn">Menu</a>
             </div>
             <div class="image">
                <img src="images/Food/Lasagna.jpg" alt="">
@@ -30,7 +30,7 @@
             <div class="content">
                <span>Order Online Now</span>
                <h3>Pepperoni Pizza</h3>
-               <a href="menu.html" class="btn">Menu</a>
+               <a href="menu.php" class="btn">Menu</a>
             </div>
             <div class="image">
                <img src="images/Food/pizza.jpg" alt="">
@@ -50,22 +50,22 @@
 
    <div class="box-container">
 
-      <a href="menu.html" class="box">
+      <a href="menu.php" class="box">
          <img src="images/Icons/apps.png" alt="Apps">
          <h3>Appetisers</h3>
       </a>
    
-      <a href="menu.html" class="box">
+      <a href="menu.php" class="box">
          <img src="images/Icons/mainD.png" alt="Entre">
          <h3>Main</h3>
       </a>
    
-      <a href="menu.html" class="box">
+      <a href="menu.php" class="box">
          <img src="images/Icons/drinks.png" alt="Drinks">
          <h3>Beverages</h3>
       </a>
    
-      <a href="menu.html" class="box">
+      <a href="menu.php" class="box">
          <img src="images/Icons/desserts.png" alt="Desserts">
          <h3>Desserts</h3>
       </a>
