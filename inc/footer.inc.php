1670a540ce3731ed34fffe55dc9063815a38228f
suffyduffy <elfiansufi@gmail.com>
2024-04-01 14:39:42 +0800

    instagram

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer-content">
       <h2 id="socials">Our Socials</h2>
-      <a href="https://www.instagram.com/coffe_ecaz/" target="_blank" aria-label="instagram"><i class="fab fa-instagram" aria-hidden="true" style="color:antiquewhite">CoffeeCatz</i></a>
+      <a href="https://www.instagram.com/coffe_ecatz/" target="_blank" aria-label="instagram"><i class="fab fa-instagram" aria-hidden="true" style="color:antiquewhite">CoffeeCatz</i></a>
       <p> CoffeeCatz@gmail.com</p>
       <p> +65 8750 9560</p>
     </div>
